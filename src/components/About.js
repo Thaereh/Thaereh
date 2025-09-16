86a9d35414f9c4a8da43a6c66976102f0db47dfa
Thae Reh <aungclayton@gmail.com>
2025-09-16 19:21:28 -0400

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="about" id="about">
+      <h2>About Me</h2>
+      <p>
+        I’m Tha Reh Aung, a passionate QA Engineer skilled in Python automation, web testing, and building robust solutions that make a difference. I enjoy tackling complex problems and delivering high-quality results.
+      </p>
+      <ul className="skills-list">
+        <li>Python</li>
+        <li>Selenium</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Automation</li>
+      </ul>
+    </section>
+  );
+}
