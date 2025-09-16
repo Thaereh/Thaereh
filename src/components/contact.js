c49f4b4d9a37b77242f855b5aae854bbe592befe
Thae Reh <aungclayton@gmail.com>
2025-09-16 19:23:53 -0400

    Create contact.js

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="contact" id="contact">
+      <h2>Contact</h2>
+      <p>
+        Let’s connect! Reach me at <a href="mailto:your.email@example.com">your.email@example.com</a>
+      </p>
+    </section>
+  );
+}
